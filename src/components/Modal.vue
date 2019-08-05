8ce75e36fc4943d90c6ff4a4dcb37f6758f53371
k1lodno <trilestino@gmail.com>
2019-08-05 10:33:44 +0300

    fix 3

---
+++ src/components/Modal.vue
@@ -0,0 +1,238 @@
+<template>
+    <transition name="modal-fade">
+        <div class="modal-backdrop">
+            <div class="modal"
+                 role="dialog"
+                 aria-labelledby="modalTitle"
+                 aria-describedby="modalDescription">
+
+                <header
+                        class="modal-header"
+                        id="modalTitle">
+                    <slot name="header">
+                        Edit
+                        <button
+                                type="button"
+                                class="btn-close"
+                                @click="close"
+                                aria-label="Close modal">x</button>
+                    </slot>
+                </header>
+
+                <section
+                        class="modal-body"
+                        id="modalDescription">
+                    <slot name="body">
+                        <label>Product Name
+                            <input type="text" id="productName" v-model="Name"></label><br>
+                        <label>Unit Price
+                            <span class="inputPrice">$<input type="number" step="1" min="1" id="unitPrice" v-model="Price"></span></label>
+                        <br>
+                        <label>Units In Stock
+                            <input type="number"  step="1" min="0" id="unitsInStock" v-model="Stock"></label>
+                        <br>
+                        <label>Discontinued
+                            <input type="checkbox" id="isDiscontinued" v-model="Disc"></label>
+                    </slot>
+                </section>
+
+                <footer class="modal-footer">
+                    <slot name="footer">
+                        <button
+                                type="button"
+                                class="bottom-btn"
+                                @click="close"
+                                aria-label="Close modal">
+                            CANCEL
+                        </button>
+                        <button
+                                type="button"
+                                class="bottom-btn"
+                                @click="update"
+                                aria-label="Update modal">
+                            UPDATE
+                        </button>
+                    </slot>
+                </footer>
+
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+
+    export default {
+        name: "Popup",
+        data(){
+            return {
+                tempName: "",
+                tempPrice: "",
+                tempStock: 0,
+                tempDisc: false,
+            }
+        },
+        props: {
+            //indexRow: Number,
+            dataRow: Array,
+            isEdit: Boolean
+        },
+        methods: {
+            close() {
+                this.$emit('close');
+            },
+            update(){
+                if((this.tempName && this.tempPrice) !== "" ) {
+                    this.$emit("productData", {
+                        productName: this.tempName,
+                        unitPrice: "$" + this.tempPrice,
+                        unitsInStock: this.tempStock,
+                        discontinued: this.tempDisc
+                    });
+                    this.$emit('close');
+                }else{
+                    alert("Fill out all the fields");
+                }
+            },
+        },
+        computed:{
+            Name: {
+                get: function () {
+                    if(this.isEdit){
+                        return this.tempName = this.dataRow[0].productName;
+                    }
+                    return this.tempName;
+                },
+                set: function (val) {
+                    return this.tempName = val;
+                }
+            },
+            Price:{
+                get: function () {
+                    if(this.isEdit){
+                        return this.tempPrice = this.dataRow[0].unitPrice.substr(1);
+                    }else {
+                        return this.tempPrice;
+                    }
+                },
+                set: function (val) {
+                    return this.tempPrice = val;
+                }
+            },
+            Stock:{
+                get: function () {
+                    if(this.isEdit){
+                        return this.tempStock = this.dataRow[0].unitsInStock;
+                    }else {
+                        return this.tempStock;
+                    }
+                },
+                set: function (val) {
+                    return this.tempStock = val;
+                }
+            },
+            Disc:{
+                get: function () {
+                    if(this.isEdit){
+                        return this.tempDisc = this.dataRow[0].discontinued;
+                    }else {
+                        return this.tempDisc;
+                    }
+                },
+                set: function (val) {
+                    return this.tempDisc = val;
+                }
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+    .modal-backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.3);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal {
+        background: #FFFFFF;
+        box-shadow: 2px 2px 20px 1px;
+        overflow-x: auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .modal-header{
+        font-family: 'Open Sans', sans-serif;
+        padding: 15px;
+        display: flex;
+        border-bottom: 1px solid #7D82A8;
+        color: #44475C;
+        justify-content: space-between;
+    }
+
+    .modal-footer {
+        padding: 15px;
+        display: flex;
+        border-top: 1px solid #7D82A8;
+        justify-content: flex-end;
+    }
+
+    .modal-body {
+        position: relative;
+        padding: 20px 10px;
+        text-align:right;
+    }
+
+    .btn-close {
+        border: none;
+        font-size: 20px;
+        cursor: pointer;
+        font-weight: bold;
+        color: #44475C;
+        background: transparent;
+    }
+
+    .bottom-btn {
+        color: white;
+        background: #44475C;
+        border: 1px solid #7D82A8;
+        border-radius: 2px;
+    }
+
+    #unitPrice, #unitPrice:hover, #unitPrice:focus, #unitPrice:active {
+        border: 0;
+        outline: none;
+        outline-offset: 0;
+        color: #555;
+        font-size: 13px;
+    }
+
+    .inputPrice {
+        border: 1px solid gray;
+        color: #999;
+        font-size: 17px;
+    }
+
+    input{
+        margin-bottom:5px;
+    }
+    .modal-fade-enter-active {
+        transition: opacity 0.5s;
+    }
+    .modal-fade-leave-active {
+        transition: opacity 0.5s;
+    }
+    .modal-fade-enter, .modal-fade-leave-to {
+        opacity: 0;
+    }
+</style>
+
---
+++ src/components/Table.vue
@@ -0,0 +1,195 @@
+<template>
+    <div>
+        <table>
+            <caption>
+                <button type="button"
+                        class="btn" @click="showModal" id="addBtn">ADD NEW RECORD</button>
+            </caption>
+
+            <Popup  v-if="isModalVisible"
+                    @close="closeModal" @productData="onData" />
+
+            <Popup  v-if="isEditModalVisible"
+                    @close="closeEditModal" @productData="onData" :dataRow="tempRow" :isEdit="isEditModalVisible"/>
+
+            <thead>
+            <tr>
+                <th>Product Name</th>
+                <th>Unit Price</th>
+                <th>Units in Stock</th>
+                <th>Discontinued</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(row, index) in getRows()">
+                <td v-for="col in columns">{{row[col]}}</td>
+                <td><button type="button" class="btn" @click="showEditModal(index, row)">EDIT</button>
+                    <div class="divider"></div>
+                    <button type="button" class="btn" @click="deleteRow(index)">DELETE</button></td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div class="pagination">
+            <div class="number"
+                 v-for="i in numPages()"
+                 v-bind:class="[i === currentPage ? 'active' : '']"
+                 v-on:click="changePage(i)">{{i}}</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Popup from './Modal'
+
+    export default {
+        name: "Table",
+        components: {
+            Popup
+        },
+        data(){
+            return {
+                rows: [],
+                isModalVisible: false,
+                isEditModalVisible: false,
+                currentPage: 1,
+                elementsPerPage: 5,
+                tempIndex: "",
+                tempRow: []
+            }
+        },
+        methods: {
+            showModal() {
+                this.isModalVisible = true;
+            },
+            showEditModal(index, row) {
+                this.isEditModalVisible = true;
+                if(row != null){
+                    this.tempIndex = index;
+                    //this.tempRow[this.tempIndex] = row;
+                    this.tempRow.splice(0,1,row);
+                }
+            },
+            closeModal() {
+                this.isModalVisible = false;
+            },
+            closeEditModal() {
+                this.isEditModalVisible = false;
+            },
+            onData(data){
+                if(this.isEditModalVisible){
+                    this.rows[this.tempIndex] = data;
+                }else {
+                    this.rows.push(data);
+                }
+            },
+            deleteRow(index){
+                if(confirm("Are you sure to delete this item?")) {
+                    if (this.currentPage !== 1) {
+                        index = index + (this.currentPage - 1) * this.elementsPerPage;
+                        this.rows.splice(index, 1);
+                    } else {
+                        this.rows.splice(index, 1);
+                    }
+                }
+            },
+            numPages() {
+                return Math.ceil(this.rows.length / this.elementsPerPage);
+            },
+            getRows() {
+                let start = (this.currentPage-1) * this.elementsPerPage;
+                let end = start + this.elementsPerPage;
+                return this.rows.slice(start, end);
+
+            },
+            changePage(page) {
+                this.currentPage = page;
+            }
+        },
+        computed: {
+            columns() {
+                if (this.rows.length === 0) {
+                    return [];
+                }
+                return Object.keys(this.rows[0])
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    table {
+        font-family: 'Open Sans', sans-serif;
+        width: 750px;
+        border-collapse: collapse;
+        border: 3px solid #44475C;
+        margin: 10px 10px 0 10px;
+    }
+
+    table th {
+        text-transform: uppercase;
+        text-align: left;
+        background: #44475C;
+        color: #FFF;
+        padding: 8px;
+        min-width: 30px;
+    }
+
+    table tbody tr {
+        border: 1px solid #44475C;
+    }
+    table td {
+        text-align: left;
+        padding: 8px;
+        border-right: 2px solid #7D82A8;
+    }
+
+    .pagination {
+        font-family: 'Open Sans', sans-serif;
+        text-align: right;
+        width: 750px;
+        padding: 8px;
+    }
+
+    .number {
+        font-weight: bold;
+        display: inline-block;
+        padding: 4px 10px;
+        color: #FFF;
+        border-radius: 4px;
+        background: #44475C;
+        margin: 0px 5px;
+        cursor: pointer;
+    }
+    .number:hover,.number.active {
+        background: #717699;
+    }
+
+    .btn{
+        border: none;
+        font-weight: bold;
+        font-family: 'Open Sans', sans-serif;
+        background: #44475C;
+        color: #FFF;
+        cursor: pointer;
+    }
+
+    .divider{
+        width:5px;
+        height:auto;
+        display:inline-block;
+    }
+
+    .btn:hover{
+        background: #717699;
+    }
+
+    #addBtn{
+        float: left;
+    }
+    caption{
+        background: #44475C;
+    }
+</style>
